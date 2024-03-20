be81040f405c6d94b32c1d2a1411c23e7c1f04b4
José Rodrigues <josemiguel443@gmail.com>
2024-03-20 15:19:12 +0000

    feat Added producer data

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from consumer import Consumer
+from producer import Producer
 from aggregator import Aggregator
 
 def manage() -> None:
@@ -6,6 +7,7 @@ def manage() -> None:
     topic_to_consume = 'data'
     topic_to_send = 'agg'
     consumer = Consumer(topic=topic_to_consume, servers=kafka_server)
+    producer = Producer(topic=topic_to_send, servers=kafka_server)
     aggregator = Aggregator()
 
     print(f"Starting management of topic {topic_to_consume}")
@@ -13,9 +15,9 @@ def manage() -> None:
         if not data:
             continue
         aggregated_data: dict = aggregator.add_data(data)
-        print(f'Added aggregated data {aggregated_data}')
         if not aggregated_data:
             continue
+        producer.send(data=aggregated_data)
 
 def main():
     manage()
---
+++ app/producer.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import json
+
+class Producer:
+    def __init__(
+        self,
+        servers: list[str],
+        topic: str = 'agg',
+    ):
+        self.__topic = topic
+        self.__producer = KafkaProducer(
+            bootstrap_servers=servers,
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+
+    def send(self, data: dict):
+        self.send_to(data=data, topic=self.__topic)
+
+    def send_to(self, data: dict, topic: str | None):
+        to_send = topic or self.__topic
+        print(f'Send to topic {to_send} with data {data}')
+        self.__producer.send(to_send, data)
