3b0adcf1e2aa643286e019633cbb5bf1275f15a7
José Rodrigues <josemiguel443@gmail.com>
2024-03-20 15:18:35 +0000

    feat integrated consumer with aggregator

--- app/main.py
+++ app/main.py
@@ -1,5 +1,24 @@
+from consumer import Consumer
+from aggregator import Aggregator
+
+def manage() -> None:
+    kafka_server = 'localhost:9092'
+    topic_to_consume = 'data'
+    topic_to_send = 'agg'
+    consumer = Consumer(topic=topic_to_consume, servers=kafka_server)
+    aggregator = Aggregator()
+
+    print(f"Starting management of topic {topic_to_consume}")
+    for data in consumer.consume():
+        if not data:
+            continue
+        aggregated_data: dict = aggregator.add_data(data)
+        print(f'Added aggregated data {aggregated_data}')
+        if not aggregated_data:
+            continue
+
 def main():
-    print('HHEEELLOOOOOO QUIIIXXX')
+    manage()
 
 if __name__ == '__main__':
     main()
